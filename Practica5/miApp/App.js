749cbb96858da82ba5e93fe5a5e47b76e85b514c
santiagoamayalpz <122042569@upq.edu.mx>
2026-06-09 13:29:25 -0600

    Componente perfil, practica 6

--- Practica5/miApp/App.js
+++ Practica5/miApp/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {Saludos} from './components/Saludos';
 import {Saludos2} from './components/Saludos2';
+import {Perfil} from './components/Perfil';
 
 //Zona 2 Main Componentes
 export default function App() {
@@ -14,6 +15,8 @@ export default function App() {
       <Saludos/>  
       <Text>----------------------------------------------</Text>
       <Saludos2/>
+      <Text>----------------------------------------------</Text>
+      <Perfil/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Practica5/miApp/components/Perfil.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native'
+
+export const Perfil = () => {
+    return (
+        <View>
+            <Text>PERFIL</Text>
+            <Text>Amaya Lopez Santiago</Text>
+            <Text>Ing. Sistemas Computacionales</Text>
+            <Text>Programacion Movil</Text>
+            <Text>Noveno Cuatrimestre</Text>
+        </View>
+    )
+}
